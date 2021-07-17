1e57c6d45ae3fa419ce9bf1d747cad64a2fa651a
c3b5aw <dev@c3b5aw.dev>
2021-07-18 00:15:45 +0200

    added more and max test

--- tests/basic_tests.c
+++ tests/basic_tests.c
@@ -6,7 +6,7 @@
 /*   By: c3b5aw <dev@c3b5aw.dev>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 23:37:38 by c3b5aw            #+#    #+#             */
-/*   Updated: 2021/07/18 00:07:02 by c3b5aw           ###   ########.fr       */
+/*   Updated: 2021/07/18 00:13:39 by c3b5aw           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,46 @@ bool	create_and_destroy_test(void)
 bool	insert_one_test(void)
 {
 	t_hashtable			*hashtable;
-	t_hashtable_item	*item;
-	char				*name1;
-	char				*name2;
 
 	hashtable = 0;
-	name1 = strdup("NAME_1");
-	name2 = strdup("NAME_2");
 	if (!test_init(&hashtable, 5, "INSERT_ONE_TEST"))
 		return (false);
-	item = hashtable_insert(&hashtable, name1, 0);
-	printf("key: %s\n", item->key);
-	item = hashtable_insert(&hashtable, name2, 0);
-	printf("key: %s\n", item->key);
+	hashtable_insert(&hashtable, strdup("NAME_5"), 0);
+	if (!test_destroy(&hashtable, "INSERT_ONE_TEST"))
+		return (false);
+	return (true);
+}
+
+bool	insert_max_test(void)
+{
+	t_hashtable			*hashtable;
+
+	hashtable = 0;
+	if (!test_init(&hashtable, 5, "INSERT_ONE_TEST"))
+		return (false);
+	hashtable_insert(&hashtable, strdup("NAME_1"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_2"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_3"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_4"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_5"), 0);
+	if (!test_destroy(&hashtable, "INSERT_ONE_TEST"))
+		return (false);
+	return (true);
+}
+
+bool	insert_more_test(void)
+{
+	t_hashtable			*hashtable;
+
+	hashtable = 0;
+	if (!test_init(&hashtable, 5, "INSERT_ONE_TEST"))
+		return (false);
+	hashtable_insert(&hashtable, strdup("NAME_1"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_2"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_3"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_4"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_5"), 0);
+	hashtable_insert(&hashtable, strdup("NAME_6"), 0);
 	if (!test_destroy(&hashtable, "INSERT_ONE_TEST"))
 		return (false);
 	return (true);
@@ -50,6 +77,10 @@ int	basic_tests(void)
 	if (!create_and_destroy_test())
 		return (false);
 	if (!insert_one_test())
+		return (false);
+	if (!insert_max_test())
+		return (false);
+	if (!insert_more_test())
 		return (false);
 	return (true);
 }
